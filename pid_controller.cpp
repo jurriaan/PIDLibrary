a0abc757994d744c61634d9bb3e610e3c811fe30
Jurriaan Pruis <email@jurriaanpruis.nl>
2015-03-23 21:40:47 +0100

    Fixes

--- pid_controller.cpp
+++ pid_controller.cpp
@@ -1,11 +1,11 @@
 #include "pid_controller.h"
 
 PIDController::PIDController(double kp, double ki, double kd, double setpoint, double kb, double smooth)
-  : d_period(1000.0), d_input(0.0), d_output(0.0), d_integral(0.0), d_derivative(0.0), d_ready(false)
+  : d_period(1000), d_period_in_sec(1.0), d_input(0.0), d_output(0.0), d_integral(0.0), d_derivative(0.0), d_ready(false)
 {
   setParameters(kp, ki, kd, kb, smooth);
   setSetpoint(setpoint);
-  setConstraints(0.0, 500.0);
+  setConstraints(0.0, 1000.0);
 }
 
 void PIDController::setSetpoint(double setpoint)
@@ -15,28 +15,21 @@ void PIDController::setSetpoint(double setpoint)
 
 void PIDController::setParameters(double kp, double ki, double kd, double kb, double smooth)
 {
-  double period_in_sec = static_cast<double>(d_period) / 1000.0;
-
   d_kp = kp;
-  d_ki = ki * period_in_sec;
-  d_kd = kd / period_in_sec;
+  d_ki = ki * d_period_in_sec;
+  d_kd = kd / d_period_in_sec;
   d_kb = kb;
   d_smooth = smooth;
 }
 
 void PIDController::setSamplePeriod(unsigned int period)
 {
+  d_period_in_sec = static_cast<double>(period) / 1000.0;
   double ratio = static_cast<double>(period) / static_cast<double>(d_period);
 
   d_ki *= ratio;
   d_kd /= ratio;
   d_period = period;
-}
-
-void PIDController::setConstraints(double min, double max)
-{
-  d_min = min;
-  d_max = max;
 }
 
 double PIDController::control(double input)
@@ -45,10 +38,11 @@ double PIDController::control(double input)
   if(d_ready)
     d_derivative = (1 - d_smooth) * d_derivative + d_kd * d_smooth * (input - d_input); // Low pass filtered derivative
   else
-  {
-    d_derivative = 0; d_ready = true;
-  }
+    { d_derivative = 0; d_ready = true; }
+
   d_integral += d_ki * d_error;
+
+  d_error *= d_kp;
 
   double output = calculateOutput();
 
--- pid_controller.h
+++ pid_controller.h
@@ -17,8 +17,9 @@ class PIDController
   double d_derivative;
   double d_min;
   double d_max;
+  double d_period_in_sec;
   unsigned int d_period;
-  double d_ready;
+  bool d_ready;
 
   public:
     PIDController(double kp, double ki, double kd, double setpoint, double kb = 1.0, double smooth = 0.25);
@@ -26,6 +27,19 @@ class PIDController
     void setConstraints(double min, double max);
     void setSetpoint(double setpoint);
     void setSamplePeriod(unsigned int period);
+    double getKp() const;
+    double getKi() const;
+    double getKd() const;
+    double getKb() const;
+    double getSmooth() const;
+    void setKp(double kp);
+    void setKi(double ki);
+    void setKd(double kd);
+    void setKb(double kb);
+    void setSmooth(double smooth);
+    double getProportional() const;
+    double getIntegral() const;
+    double getDerivative() const;
     double control(double input);
 
   private:
@@ -45,6 +59,22 @@ inline double PIDController::constrain(double val)
 
 inline double PIDController::calculateOutput()
 {
-  return d_kp * d_error + d_integral + d_derivative;
+  return d_error + d_integral - d_derivative;
 }
+
+inline double PIDController::getKp() const { return d_kp; }
+inline double PIDController::getKi() const { return d_ki / d_period_in_sec; }
+inline double PIDController::getKd() const { return d_kd * d_period_in_sec; }
+inline double PIDController::getKb() const { return d_kb; }
+inline double PIDController::getSmooth() const { return d_smooth; }
+inline void PIDController::setKp(double kp) { d_kp = kp; }
+inline void PIDController::setKi(double ki) { d_ki = ki * d_period_in_sec; }
+inline void PIDController::setKd(double kd) { d_kd = kd / d_period_in_sec; }
+inline void PIDController::setKb(double kb) { d_kb = kb; }
+inline void PIDController::setSmooth(double smooth) { d_smooth = smooth; }
+inline void PIDController::setConstraints(double min, double max) { d_min = min; d_max = max; }
+inline double PIDController::getProportional() const { return d_error; }
+inline double PIDController::getIntegral() const { return d_integral; }
+inline double PIDController::getDerivative() const { return d_derivative; }
+
 #endif
